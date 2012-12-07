a391927dbcc0f24fd8b51e746e3e73f62b25fc22
Priscila Mayumi Sato <mayumisatox@gmail.com>
2012-12-07 03:54:20 -0200

    create a db contex, use a dbContext to create a database and the data into, and select getting the SQL code

---
+++ Tracing-Query/Tracing-Code-First/MContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace Tracing_Code_First
+{
+    public class MContext : DbContext
+    {
+        public DbSet<Manga> Mangas { get; set; }
+    }
+}
---
+++ Tracing-Query/Tracing-Code-First/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Objects;
+
+namespace Tracing_Code_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MContext db = new MContext())
+            {
+                Manga manga = new Manga();
+                manga.Title = "Saint Seiya";
+                manga.Description = "The battle of Athena and Hades";
+
+                db.Mangas.Add(manga);
+                db.SaveChanges();
+            }
+
+            Console.ReadLine();
+
+            using (MContext db = new MContext())
+            {
+                var result = (from m in db.Mangas
+                              where m.Id.Equals(1)
+                              select m);
+                Console.WriteLine(((System.Data.Entity.Infrastructure.DbQuery<Manga>)result).ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
