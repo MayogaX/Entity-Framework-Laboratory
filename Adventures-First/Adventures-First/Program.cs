49204d9b6a099535d77eab389ed0efdb240ee9b8
Priscila Mayumi Sato <mayumisatox@gmail.com>
2013-01-17 22:49:46 -0200

    Exemple about database first using the AdventuresWorks

--- Adventures-First/Adventures-First/Program.cs
+++ Adventures-First/Adventures-First/Program.cs
@@ -9,6 +9,26 @@ namespace Adventures_First
     {
         static void Main(string[] args)
         {
+            AdventureWorks2008Entities context = new AdventureWorks2008Entities();
+
+            //Address address = new Address();
+            //address.AddressLine1 = "Rua dos Bobos, 0";
+            //address.AddressLine2 = "Bairro do Morais";
+            //address.City = "Vinicius";
+            //address.PostalCode = "00000-000";
+            //address.StateProvinceID = 1;
+            //address.rowguid = new Guid();
+            //address.ModifiedDate = DateTime.Now;
+            //context.AddToAddresses(address);
+
+            //Address address = (from a in context.Addresses where a.AddressID == 32525 select a).First();
+            //address.City = "Rio de Janeiro";
+
+            Address address = context.Addresses.Single(a => a.AddressID == 32525);
+            context.Addresses.DeleteObject(address);
+
+            context.SaveChanges();
+
         }
     }
 }
