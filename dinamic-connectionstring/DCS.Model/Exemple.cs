e7940b815bcfed71c7b8ebee76a16834ebb84b7e
Priscila Mayumi Sato <mayumisatox@gmail.com>
2013-02-13 16:07:11 -0200

    adicionada camada de negocios, feitos alguns testes, adicionado modelo de exemplo

---
+++ dinamic-connectionstring/DCS.Model/Exemple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DCS.Model
+{
+    public class Exemple
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public User User { get; set; }
+
+        public Exemple(String title)
+        {
+            this.Title = title;
+            this.Id = new Guid();
+        }
+    }
+}
--- dinamic-connectionstring/DCS.Model/User.cs
+++ dinamic-connectionstring/DCS.Model/User.cs
@@ -11,5 +11,13 @@ namespace DCS.Model
         public String Name { get; set; }
         public String Login { get; set; }
         public String Passaword { get; set; }
+
+        public User(string name, string login, string passaword)
+        {
+            this.Id = new Guid();
+            this.Name = name;
+            this.Login = login;
+            this.Passaword = passaword;
+        }
     }
 }
