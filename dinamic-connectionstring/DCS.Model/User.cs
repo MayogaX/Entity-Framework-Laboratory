23643e6f30118115857f50d02a67163a659c02ea
Priscila Mayumi Sato <mayumisatox@gmail.com>
2013-02-13 15:47:48 -0200

    add the project exemple for dinamic connection string

---
+++ dinamic-connectionstring/DCS.Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DCS.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Login { get; set; }
+        public String Passaword { get; set; }
+    }
+}
